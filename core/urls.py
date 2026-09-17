886ef67193341ed2cb366151877cb3a61d8cf7e2
amrrelabd <amr12elabd@gmail.com>
2026-09-17 16:19:43 +0300

    Make the app installable as a PWA
    Adds a manifest, app icons, and a service worker so the site can be
    added to a phone's home screen and opens full-screen like a native app.

--- core/urls.py
+++ core/urls.py
@@ -14,13 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.http import HttpResponse
 from django.urls import include, path
+
+
+def service_worker(request):
+    path = settings.BASE_DIR / 'inventory' / 'static' / 'inventory' / 'sw.js'
+    with open(path, 'rb') as f:
+        content = f.read()
+    response = HttpResponse(content, content_type='application/javascript')
+    response['Service-Worker-Allowed'] = '/'
+    return response
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('sw.js', service_worker, name='service_worker'),
     path('', include('inventory.urls')),
 ]
